eaad21d823238f16c79cb9b4f6083e068891e6a0
zihad868 <zihadh868@gmail.com>
2023-12-12 22:33:34 +0600

    Binary Tree

---
+++ Binary Search/Binary Tree.py
@@ -0,0 +1,25 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        srightleft = None
+        self.right = None
+
+
+# Example binary tree:
+#         1
+#       /   \
+#      2     3
+#     / \   / \
+#    4   5 6   7
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+print(root.right.right.value)
